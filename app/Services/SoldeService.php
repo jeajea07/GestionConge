40349ac6f0b284841c96f50aa5884660b96f99bc
noraarandrianarivelo <mahatsangyaaron.randrianarivelo@outlook.com>
2026-05-13 11:30:42 +0300

    fonction espace-employee avec les validations

--- app/Services/SoldeService.php
+++ app/Services/SoldeService.php
@@ -4,16 +4,19 @@ namespace App\Services;
 
 // Import des Models
 use App\Models\SoldeModel;
+use App\Models\TypeCongeModel;
 
 use DateTime;
 
 class SoldeService
 {
     protected $soldeModel;
+    protected $typeCongeModel;
 
     public function __construct()
     {
         $this->soldeModel = new SoldeModel();
+        $this->typeCongeModel = new TypeCongeModel();
     }
 
     public function calculerJourRestant(int $employeId, int $typeCongeId, int $annee): int{
@@ -23,7 +26,45 @@ class SoldeService
                                   ->first();
 
         $restant = $solde['jour_attribues'] - $solde['jour_pris'];      
-                
+
         return $restant;
     }
+
+    public function getSoldeRestantByEmployeIdAndByTypeCongeId(int $employeId, int $typeCongeId, int $annee){
+        $solde = $this->soldeModel->where('employe_id', $employeId)
+                                  ->where('type_conge_id', $typeCongeId)
+                                  ->where('annee', $annee)
+                                  ->first();
+
+        $typeConge = $this->typeCongeModel->find($typeCongeId);
+
+        if(!$solde){
+            return null;
+        }
+
+        $restant = $solde['jour_attribues'] - $solde['jour_pris'];      
+
+        return [
+            'employe_id' => $employeId,
+            'type_conge_' => $typeConge['libelle'],
+            'annee' => $annee,
+            'jour_attribues' => $solde['jour_attribues'],
+            'jour_pris' => $solde['jour_pris'],
+            'jour_restant' => $restant
+        ];
+    }
+
+    public function getAllSoldeRestantByEmployeIdAndByTypeCongeId(int $employeId, int $annee){
+        $typeConge = $this->typeCongeModel->findAll();
+
+        $result = [];
+        foreach($typeConge as $type){
+            $solde = $this->getSoldeRestantByEmployeIdAndByTypeCongeId($employeId, $type['id'], $annee);
+            if($solde){
+                $result[] = $solde;
+            }
+        }
+        return $result;
+    }
+
 }
